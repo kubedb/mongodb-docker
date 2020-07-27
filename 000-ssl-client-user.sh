7428c067676faac065d808e10ca867613aaeffba
Tamal Saha <tamal@appscode.com>
2020-07-26 21:23:21 -0700

    Format script
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- 000-ssl-client-user.sh
+++ 000-ssl-client-user.sh
@@ -28,14 +28,14 @@ client_pem="${MONGO_CLIENT_CERTIFICATE_PATH:-/var/run/mongodb/tls/client.pem}"
 ca_crt="${MONGO_CA_CERTIFICATE_PATH:-/var/run/mongodb/tls/ca.crt}"
 
 if [[ ${SSL_MODE} != "disabled" ]] && [[ -f "$client_pem" ]] && [[ -f "$ca_crt" ]]; then
-  admin_user="${MONGO_INITDB_ROOT_USERNAME:-}"
-  admin_password="${MONGO_INITDB_ROOT_PASSWORD:-}"
-  admin_creds=(-u "$admin_user" -p "$admin_password" --authenticationDatabase admin)
-  ssl_args=(--tls --tlsCAFile "$ca_crt" --tlsCertificateKeyFile "$client_pem")
+    admin_user="${MONGO_INITDB_ROOT_USERNAME:-}"
+    admin_password="${MONGO_INITDB_ROOT_PASSWORD:-}"
+    admin_creds=(-u "$admin_user" -p "$admin_password" --authenticationDatabase admin)
+    ssl_args=(--tls --tlsCAFile "$ca_crt" --tlsCertificateKeyFile "$client_pem")
 
-  user=$(openssl x509 -in "$client_pem" -inform PEM -subject -nameopt RFC2253 -noout)
-  # remove prefix 'subject= ' or 'subject='
-  user=$(echo ${user#"subject="})
-  echo "Creating root user $user for SSL..." #xref: https://docs.mongodb.com/manual/tutorial/configure-x509-client-authentication/#procedures
-  mongo admin --host localhost "${admin_creds[@]}" "${ssl_args[@]}" --eval "db.getSiblingDB(\"\$external\").runCommand({createUser: \"$user\",roles:[{role: 'root', db: 'admin'}],})"
+    user=$(openssl x509 -in "$client_pem" -inform PEM -subject -nameopt RFC2253 -noout)
+    # remove prefix 'subject= ' or 'subject='
+    user=$(echo ${user#"subject="})
+    echo "Creating root user $user for SSL..." #xref: https://docs.mongodb.com/manual/tutorial/configure-x509-client-authentication/#procedures
+    mongo admin --host localhost "${admin_creds[@]}" "${ssl_args[@]}" --eval "db.getSiblingDB(\"\$external\").runCommand({createUser: \"$user\",roles:[{role: 'root', db: 'admin'}],})"
 fi
